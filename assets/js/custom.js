c828c512f789c5e3eb9fe34ff877af25c015d41f
rojab <irojabkhan@gmail.com>
2021-07-03 02:49:59 +0600

    trending added

---
+++ assets/js/custom.js
@@ -0,0 +1,65 @@
+
+$( document ).ready(function() {
+    "use strict"
+
+    /* Favourite Settings
+    ..........................................*/
+    $('.trading__card .favourite').on('click', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('isFav');
+    })
+
+    /* CountDown Js
+    ..........................................*/
+    $('[data-countdown]').each(function() {
+        var $this = $(this), finalDate = $(this).data('countdown');
+        $this.countdown(finalDate, function(event) {
+            var totalHours = event.offset.totalDays;
+            $(this).html(event.strftime(totalHours + 'D &nbsp; %HH &nbsp; %MM &nbsp; LEFT'));
+        });
+      });
+
+    /* OwlCarousel
+    ..........................................*/
+      $(".trending__card__carousel").owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+        dots: false,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+      });
+
+    /* Marquee Js
+    ..........................................*/
+    function hello() {
+        $('.top__chart').marquee({
+            duration: 5000,
+            gap: 50,
+            delayBeforeStart: 0,
+            direction: 'left',
+            pauseOnHover: true,
+            speed: 100,
+            duplicated: false
+        });
+    }
+    hello();
+
+    $('.top__chart__wrap .nav-link').on('show.bs.tab', function (e) {
+        e.preventDefault();
+        hello();
+    });
+
+});
